7e5fac9388a911ec10f28fe86061f2217232c3fd
infinixs5lite <kelvinchinedu97@gmail.com>
2022-11-21 20:40:32 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/infinix/Infinix-X689C
+DEVICE_PATH := device/infinix/X689C
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
