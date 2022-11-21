50fab399ba812f33bcbab2ad1870df82166e60a0
infinixs5lite <kelvinchinedu97@gmail.com>
2022-11-21 20:50:45 +0100

    Update and rename omni_Infinix-X689C.mk to twrp_X689C.mk

--- twrp_X689C.mk
+++ twrp_X689C.mk
@@ -7,18 +7,21 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Inherit from Infinix-X689C device
-$(call inherit-product, device/infinix/Infinix-X689C/device.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
-PRODUCT_DEVICE := Infinix-X689C
-PRODUCT_NAME := omni_Infinix-X689C
+# Inherit from X689C device
+$(call inherit-product, device/infinix/X689C/device.mk)
+
+PRODUCT_DEVICE := X689C
+PRODUCT_NAME := twrp_X689C
 PRODUCT_BRAND := Infinix
-PRODUCT_MODEL := Infinix X689C
+PRODUCT_MODEL := Infinix hot 10T
 PRODUCT_MANUFACTURER := infinix
 
 PRODUCT_GMS_CLIENTID_BASE := android-infinix
